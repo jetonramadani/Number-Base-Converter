4a8abc3fd235ba70dd4ecd3d3f03fae1dd3956bc
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-25 22:00:47 +0200

    Work on project. Stage 1/4: Convert decimals

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,8 +1,17 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // write your code here
+        System.out.print("Enter number in decimal system: ");
+        long numDecimal = sc.nextLong();
+        System.out.print("Enter target base: ");
+        int radix = sc.nextInt();
+        System.out.printf("Conversion result: %s%n",
+                Long.toString(numDecimal, radix));
     }
 }
