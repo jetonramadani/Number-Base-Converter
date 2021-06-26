913b4dc035d9c5c50d5dc36b748699ecd1727a8b
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-26 16:29:32 +0200

    Work on project. Stage 4/4: Convert fractions

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,10 +1,19 @@
 package converter;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
+    private static int getValue(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return ch - '0';
+        } else {
+            return Character.toUpperCase(ch) - 'A' + 10;
+        }
+    }
     private static void convert(String[] bases) {
         int from = Integer.parseInt(bases[0]);
         int to = Integer.parseInt(bases[1]);
@@ -14,12 +23,42 @@ public class Main {
             if (line.equals("/back")) {
                 break;
             }
-            BigInteger integer = new BigInteger(line, from);
-            System.out.println("Conversion result: " + integer.toString(to));
+            String[] parts = line.split("\\.");
+            BigInteger integer = new BigInteger(parts[0], from);
+            String answer = "Conversion result: " + integer.toString(to);
+            if (parts.length == 2) {
+                BigDecimal divide = new BigDecimal(String.valueOf(from));
+                BigDecimal multiplier = BigDecimal.ONE.divide(divide, 5, RoundingMode.HALF_UP);
+                BigDecimal res = BigDecimal.ZERO;
+                for (char ch : parts[1].toCharArray()) {
+                    res = res.add(new BigDecimal(getValue(ch)).multiply(multiplier));
+                    multiplier = multiplier.divide(divide, 5, RoundingMode.HALF_UP);
+                }
+                StringBuilder decPart = new StringBuilder();
+                multiplier = new BigDecimal(String.valueOf(to));
+                for (int i = 0; i < 5; i++) {
+                    res = res.multiply(multiplier);
+                    int wholePart = res.toBigInteger().intValue();
+                    res = res.subtract(new BigDecimal(wholePart));
+                    decPart.append(getCharRep(wholePart));
+                }
+                answer += "." + decPart;
+            }
+            System.out.println(answer);
         }
 
     }
-        public static void main(String[] args) {
+
+    private static char getCharRep(int wholePart) {
+        if (wholePart < 10) {
+            return (char) (wholePart + '0');
+        } else {
+            return (char) ('a' + wholePart - 10);
+
+        }
+    }
+
+    public static void main(String[] args) {
         boolean flag = true;
         // write your code here
         while(flag) {
